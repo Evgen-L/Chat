cf4a388f5b36b75278b7fec14d0f11a53d72438d
Калинин Данила <danilakalina9@gmail.com>
2022-12-20 22:31:01 +0300

    add tests

---
+++ backend/tests/Test.cs
@@ -0,0 +1,15 @@
+using XUnit
+
+namespace Tests;
+
+public class Tests
+{
+    [Fact]
+    public void CheckGetConnString()
+    {
+        string expected = GetConnectionString();
+        string actual = "Host={localhost};Username={postgres};Password={admin};Database={chat_db}"
+
+        Assert.Equal(expected, actual);
+    }
+}
