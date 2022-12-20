f381ab6a471ef3afe98b29e06f6759b5e89c176b
Калинин Данила <danilakalina9@gmail.com>
2022-12-20 23:31:27 +0300

    check test 10

---
+++ backend/tests/Test.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using backend.DbTools;
+
+namespace Tests;
+
+public class Tests
+{
+    [Fact]
+    public void CheckGetConnString()
+    {
+        string expected = GetConnectionString();
+        string actual = "Host=localhost;Username=postgres;Password=admin;Database=chat_db";
+
+        Assert.Equal(expected, actual);
+    }
+}
