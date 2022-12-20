5e64e6542cbb16b9fc8d8367aab9dc9d0a7a8cb7
Evgeniy <e.liakishev@gmail.com>
2022-12-20 20:59:25 +0300

    Переместил папку с тестами в нужное место. Проверка запуска фронт-тестов при пушинге 3

--- client/tests/calculations.ts
+++
@@ -1,4 +0,0 @@
-export const sum = (a: number, b: number) => a + b
-export const subtraction = (a: number, b: number) => a - b
-export const multiplier = (a: number, b: number) => a * b
-export const division = (a: number, b: number) => a / b
